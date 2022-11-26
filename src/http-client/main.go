89f28d0e1492fae159d23b64dad21df7f0b329a2
Luis Alberto Zagonel Pozenato <luis.pozenato.cit@naturapay.net>
2022-11-26 16:01:55 -0300

    feat: :sparkles: af type on nef registration

--- src/http-client/main.go
+++ src/http-client/main.go
@@ -5,14 +5,16 @@ import "fmt"
 type NEFPostRequest struct {
 	Id   string `json:"id"`
 	Addr string `json:"addr"`
+	Type string `json:"type"`
 }
 
 func PostNEFSubscription() {
 	const notifyUrl = "http://localhost:3333/notify"
 	const id = "54321"
 	const NEFSubscriptionURI = "/subscriptions"
+	const AFType = "type_1"
 
-	body := NEFPostRequest{id, notifyUrl}
+	body := NEFPostRequest{id, notifyUrl, AFType}
 
 	isOk := Post(CoreUrl+NEFSubscriptionURI, body)
 
